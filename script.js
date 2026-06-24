d3504f8b9006112347dfe2df21e50f5711a2c89e
mayuresh-04-queue <mayureshugale04@gmail.com>
2026-06-24 12:53:34 +0530

    Fixed mobile navbar

--- script.js
+++ script.js
@@ -87,4 +87,17 @@
     }, { passive: true });
   }
 
+  const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+if(menuToggle && navLinks){
+
+    menuToggle.addEventListener('click', function(){
+
+        navLinks.classList.toggle('active');
+
+    });
+
+}
+
 })();
